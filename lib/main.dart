2c4434ee6f15a0ce98f9c1c53c06e1f462cbf691
mfarel <mfarel@openpass.com.ar>
2024-06-26 11:40:08 -0300

    change login initialize

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,17 @@ void main() async {
     try {
       print('entro al try');
       print(FIRE_API_KEY);
+      print(Firebase.apps.isEmpty);
       if (Firebase.apps.isEmpty) {
         print('firebase empty');
-        await Firebase.initializeApp(
-          options: const FirebaseOptions(
-            apiKey: FIRE_API_KEY,
-            authDomain: FIRE_AUTH_DOMAIN,
-            storageBucket: FIRE_STORAGE_BUCKET,
-            appId: FIRE_APP_ID,
-            messagingSenderId: FIRE_MESSAGING_SENDER_ID,
-            projectId: FIRE_PROYECT_ID,
-          ),
-        );
+        await initializeFirebase();
       }
-    } on FirebaseException catch (e) {
+    } catch (e) {
       print('firebase error');
-      if (e.code == 'not-initialized') {
-        await Firebase.initializeApp(
-          options: const FirebaseOptions(
-            apiKey: FIRE_API_KEY,
-            authDomain: FIRE_AUTH_DOMAIN,
-            storageBucket: FIRE_STORAGE_BUCKET,
-            appId: FIRE_APP_ID,
-            messagingSenderId: FIRE_MESSAGING_SENDER_ID,
-            projectId: FIRE_PROYECT_ID,
-          ),
-        );
-      }
+
+      await initializeFirebase();
+      // if (e.code == 'not-initialized') {
+      // }
     }
     runApp(const MyApp());
   } catch (e) {
@@ -71,4 +55,17 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+Future initializeFirebase() async {
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: FIRE_API_KEY,
+      authDomain: FIRE_AUTH_DOMAIN,
+      storageBucket: FIRE_STORAGE_BUCKET,
+      appId: FIRE_APP_ID,
+      messagingSenderId: FIRE_MESSAGING_SENDER_ID,
+      projectId: FIRE_PROYECT_ID,
+    ),
+  );
 }
