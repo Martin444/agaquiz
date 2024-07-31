90650b5c020278f6b57ae074b103f1585ba0b589
mfarel <mfarel@openpass.com.ar>
2024-07-30 22:51:19 -0300

    fix scroll login

--- lib/features/Login/presentation/page/login_page.dart
+++ lib/features/Login/presentation/page/login_page.dart
@@ -24,122 +24,130 @@ class LoginPage extends ConsumerWidget {
         body: Stack(
           children: [
             const BackgroundStars(),
-            Center(
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SingleChildScrollView(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const SizedBox(
-                          height: 80,
-                        ),
-                        ElasticIn(
-                          delay: const Duration(milliseconds: 100),
-                          child: Image.asset(
-                            'assets/quizmalogo.png',
-                            height: 180,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        FadeInUp(
-                          from: 30,
-                          duration: const Duration(milliseconds: 200),
-                          delay: const Duration(milliseconds: 1000),
-                          child: Text(
-                            '¡Bienvenido a Quizma!',
-                            textAlign: TextAlign.center,
-                            style: AqTextStyle.primaryTextStyle,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        FadeInUp(
-                          from: 30,
-                          delay: const Duration(milliseconds: 1200),
-                          child: TextInputPrincipal(
-                            hintText: 'Ingresá tu nombre',
-                            inputType: TextInputType.name,
-                            inputAction: TextInputAction.continueAction,
-                            controller: loginstate.nameController,
-                            maxLines: 1,
-                            onSubmmited: (value) {
-                              try {
-                                var isEqual = loginfunctions.postNameUser();
-                                if (!isEqual) {
+                        Column(
+                          children: [
+                            const SizedBox(
+                              height: 80,
+                            ),
+                            ElasticIn(
+                              delay: const Duration(milliseconds: 100),
+                              child: Image.asset(
+                                'assets/quizmalogo.png',
+                                height: 180,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            FadeInUp(
+                              from: 30,
+                              duration: const Duration(milliseconds: 200),
+                              delay: const Duration(milliseconds: 1000),
+                              child: Text(
+                                '¡Bienvenido a Quizma!',
+                                textAlign: TextAlign.center,
+                                style: AqTextStyle.primaryTextStyle,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            FadeInUp(
+                              from: 30,
+                              delay: const Duration(milliseconds: 1200),
+                              child: TextInputPrincipal(
+                                hintText: 'Ingresá tu nombre',
+                                inputType: TextInputType.name,
+                                inputAction: TextInputAction.continueAction,
+                                controller: loginstate.nameController,
+                                maxLines: 1,
+                                onSubmmited: (value) {
                                   try {
-                                    Beamer.of(context).beamToNamed(
-                                      '/iniciar',
-                                      data: {'username': USER_NAME},
-                                    );
-                                    return;
+                                    var isEqual = loginfunctions.postNameUser();
+                                    if (!isEqual) {
+                                      try {
+                                        Beamer.of(context).beamToNamed(
+                                          '/iniciar',
+                                          data: {'username': USER_NAME},
+                                        );
+                                        return;
+                                      } catch (e) {
+                                        print(e);
+                                      }
+                                    }
+
+                                    if (isEqual) context.beamToNamed('/admin');
                                   } catch (e) {
                                     print(e);
                                   }
-                                }
-
-                                if (isEqual) context.beamToNamed('/admin');
-                              } catch (e) {
-                                print(e);
-                              }
-                            },
-                          ),
+                                },
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                    Column(
-                      children: [
-                        FadeInUp(
-                          from: 30,
-                          delay: const Duration(milliseconds: 1500),
-                          child: ButtonPrimary(
-                            title: 'Continuar',
-                            onPressed: () {
-                              try {
-                                var isEqual = loginfunctions.postNameUser();
-                                if (!isEqual) {
-                                  try {
-                                    Beamer.of(context).beamToNamed(
-                                      '/iniciar',
-                                      data: {'username': USER_NAME},
-                                    );
-                                    return;
-                                  } catch (e) {
-                                    print(e);
-                                  }
-                                }
-
-                                if (isEqual) context.beamToNamed('/admin');
-                              } catch (e) {
-                                print(e);
-                              }
-                            },
-                            load: false,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        FadeInUp(
-                          delay: const Duration(milliseconds: 1200),
-                          child: Text(
-                            'Versión: $VERSION_RELEASE',
-                            style: AqTextStyle.textButtonStyle1,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      FadeInUp(
+                        from: 30,
+                        delay: const Duration(milliseconds: 1500),
+                        child: ButtonPrimary(
+                          title: 'Continuar',
+                          onPressed: () {
+                            try {
+                              var isEqual = loginfunctions.postNameUser();
+                              if (!isEqual) {
+                                try {
+                                  Beamer.of(context).beamToNamed(
+                                    '/iniciar',
+                                    data: {'username': USER_NAME},
+                                  );
+                                  return;
+                                } catch (e) {
+                                  print(e);
+                                }
+                              }
+
+                              if (isEqual) context.beamToNamed('/admin');
+                            } catch (e) {
+                              print(e);
+                            }
+                          },
+                          load: false,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      FadeInUp(
+                        delay: const Duration(milliseconds: 1200),
+                        child: Text(
+                          'Versión: $VERSION_RELEASE',
+                          style: AqTextStyle.textButtonStyle1,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
